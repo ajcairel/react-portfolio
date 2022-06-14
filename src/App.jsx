43f6a0c0a1a3e096b848a9e708f81ba228c5ce6f
ajcairel <alexanderjcairel@gmail.com>
2022-06-14 16:44:43 -0700

    Components & Mail

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,50 @@
 import logo from './logo.svg';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+import Contact from './components/Contact';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Portfolio
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          
-        </a>
-      </header>
-    </div>
+
+    <main className="text-gray-400 bg-green-600 body-font">
+    <Navbar />
+    <About />
+    <Projects />
+    <Skills />
+    {/* <Testimonials /> */}
+    <Contact />
+  </main>
+
+  //   <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+  //   <p className="text-3xl text-gray-700 font-bold mb-5">
+  //     Welcome!
+  //   </p>
+  //   <p className="text-gray-500 text-lg">
+  //     React and Tailwind CSS in action
+  //   </p>
+  // </div>
+    // <main className='text-gray-400 bg-gray-900 body-font'>
+    // <div className="App">
+    //   <Navbar />
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Portfolio
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //     </a>
+    //     <About />
+    //   </header>
+    // </div>
+    // </main>
   );
 }
 
