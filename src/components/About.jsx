e360eac738a4b35bee6bd492a1925c04a4cb8557
ajcairel <alexanderjcairel@gmail.com>
2022-06-30 18:18:18 -0700

    Updated Resume Againx2

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -38,7 +38,7 @@ export default function About() {
             </div>
             <div className="p-2 w-full">
               <a
-                href="https://drive.google.com/file/d/1uWbAy6DTOouknxTIBe_HTCua6yZr87SJ/view?usp=sharing"
+                href="https://drive.google.com/file/d/1DCWI7o_T91z_FwPjxyMbzn1XVEVJ1t6W/view?usp=sharing"
                 target="_blank"
                 rel="noopener"
               >
